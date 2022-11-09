40e413be66b44987b424cc7fc0da5ef1e2feb37b
ddh96 <ddh963963@naver.com>
2022-11-09 15:14:06 +0900

    build : 프로세스 종료 로직 수정

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,16 @@ echo "> build 파일명: $JAR_NAME"
 
 # 흐름 서버 1 업데이트 시작 하고 -> nginx 막기(근데 롤링방식이라 서버끊기면 자동으로 바뀜) -> 자연스럽게 두번째
 echo "> 현재 구동중인 Set 확인"
-IP1=10.41.183.61
-IP2=10.41.135.168
+#비공개 ip
+IP1=# ip
+IP2=# ip
 MY_IP=$(hostname -i)
 OTHER_IP
+loop=1
+limitLoop=30
+flag='false'
 
-# 쉬고 있는 set 찾기: set1이 사용중이면 set2가 쉬고 있고, 반대면 set1이 쉬고 있음
+
 if [ $MY_IP == $IP1 ]; then
   OTHER_IP=$IP2
 elif [ $MY_IP == $IP2 ]; then
@@ -18,53 +22,91 @@ else
   echo "> 일치하는 IP가 없습니다. "
 fi
 
-echo 내 "ip" $MY_IP
-echo 내 "OTHER_IP" $OTHER_IP
+echo 내 "> ip" $MY_IP
+echo 내 "> OTHER_IP" $OTHER_IP
 
-echo "서버 체크 시작"
 
+#===================================살아있는 서버확인======================================
+echo "> 서버 체크 시작"
 for retry_count in {1..10};
 do
   response=$(sudo curl -s http://$OTHER_IP:8080/actuator/health)
   up_count=$(echo $response | grep 'UP' | wc -l)
-  echo "$retry_count : $response  : $up_count"
-  echo "나의 $MY_IP 날리는 중 http://$OTHER_IP:8080/actuator/health"
-  if [ $up_count -ge 1 ]; then # $up_count >= 1 ("UP" 문자열이 있는지 검증)
-    echo "접속 성공 ===================="
+  echo "> $retry_count : $response  : $up_count"
+  echo "> 현재서버IP : $MY_IP 타서버 IP : $OTHER_IP"
+  if [ $up_count -ge 1 ]; then
+    echo "> 서버 체크 성공"
     break
   fi
   if [ $retry_count -eq 10 ]; then
-    echo "실패"
+    echo "> 실패"
     exit 1
   fi
-  echo "실패 50초후 재시도"
+  echo "> 실패 50초후 재시도"
   sleep 5
   #다른 서버 업데이트
 done
 
-echo ">  구동중인 애플리케이션 pid 확인"
-IDLE_PID=$(sudo ps -ef | grep $JAR_NAME | grep -v 'grep' | awk '{print $2}')
-echo "> 제거할 pid $IDLE_PID"
-if [ -z $IDLE_PID ]; then
+#===================================프로세스 종료======================================
+# tomcat gracefully shutdown
+echo "> 구동중인 애플리케이션 pid 확인"
+IDLE_PID=(`ps -ef | grep  $JAR_NAME | grep -v 'grep' | awk '{ print $2 }'`)
+ if [ ${#IDLE_PID[@]} = 0 ]
+then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  flag='true'
 else
-  echo "> kill -15 $IDLE_PID"
-  sudo kill -15 $IDLE_PID
-  sleep 10
+  for pid in "${IDLE_PID[@]}"
+  do
+      echo "> [$pid] gracefully shutdown"
+      kill -15 $pid
+  done
+  while [ $loop -le $limitLoop ]
+  do
+      PID_LIST=(`ps -ef | grep  $JAR_NAME | grep -v 'grep' | awk '{ print $2 }'`)
+      if [ ${#PID_LIST[@]} = 0 ]
+      then
+          echo "> gracefully shutdown success "
+          flag='true'
+          break
+      else
+          for pid in "${PID_LIST[@]}"
+          do
+              echo "> [$loop/$limitLoop] $pid 프로세스 종료를 기다리는중입니다."
+          done
+          loop=$(( $loop + 1 ))
+          sleep 1
+          continue
+      fi
+  done
+fi
+if [ $flag == 'false' ];
+then
+    echo "> 프로세스 강제종료 시도"
+    sudo ps -ef | grep $JAR_NAME | grep -v 'grep' |  awk '{ print $2 }' | \
+    while read PID
+    do
+        echo "> [$PID] forced shutdown"
+        kill -9 $PID
+    done
 fi
 
+
+#===================================배포======================================
 echo "> 배포"
-echo "파일명" $BASE_PATH$JAR_NAME
+echo "> 파일명" $BASE_PATH$JAR_NAME
 sudo nohup java -jar -Dspring.profiles.active=set1 $BASE_PATH$JAR_NAME &
 sudo sleep 10
 
 echo "> 10초 후 Health check 시작"
 echo "> curl -s http://$MY_IP:8080/actuator/health"
 
+
+#===================================서버 Health check  ======================================
 for retry_count in {1..10}; do
   response=$(sudo curl -s http://$MY_IP:8080/actuator/health)
   up_count=$(echo $response | grep 'UP' | wc -l)
-  if [ $up_count -ge 1 ]; then # $up_count >= 1 ("UP" 문자열이 있는지 검증)
+  if [ $up_count -ge 1 ]; then
     echo "> Health check 성공"
     break
   else
@@ -82,5 +124,6 @@ for retry_count in {1..10}; do
   sudo sleep 10
 done
 
+sleep 60 # 다음 배포서버 지연
 
 #JENKINS_NODE_COOKIE=dontKillMe
